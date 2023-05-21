5a3022b45cb41e802d042eb447a4fa94ae87a7f3
Hendrik de Jonge <hendrikdejonge@hotmail.com>
2023-05-21 16:09:54 +0200

    Return url working, with also tabs

---
+++ AccuFin/Services/PreviousNavigationService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Web;
+
+namespace AccuFin.Services
+{
+    public class PreviousNavigationService
+    {
+        private NavigationManager _navigationManager;
+
+        public PreviousNavigationService(NavigationManager navigationManager)
+        {
+            _navigationManager = navigationManager;
+        }
+
+        public string GetReturnUrl()
+        {
+            if (_navigationManager.TryGetQueryString<string>("returnurl", out string parametersForReturn))
+            {
+                return parametersForReturn;
+            }
+            return null;
+        }
+
+        public string GetUrlQuery()
+        {
+            return "?returnurl=" + HttpUtility.UrlEncode(new Uri(_navigationManager.Uri).Query);
+        }
+    }
+}
