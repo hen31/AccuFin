c33ce0f89caa7c9cfeea9a9e1c782ae2558e7817
Hendrik de Jonge <hendrikdejonge@hotmail.com>
2023-05-30 21:18:51 +0200

    Load more history

---
+++ AccuFin.Api/Services/BankIntegration/AgreementResultModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AccuFin.Api.Services.BankIntegration
+{
+    public class AgreementResultModel
+    {
+        public string Id { get; set; }
+        public DateTime Created { get; set; }
+        public int Max_historical_days { get; set; }
+        public int Access_valid_for_days { get; set; }
+        public string[] Access_scope { get; set; }
+        public string Accepted { get; set; }
+        public string Institution_id { get; set; }
+    }
+}
--- AccuFin.Api/Services/BankIntegration/NordigenClient.cs
+++ AccuFin.Api/Services/BankIntegration/NordigenClient.cs
@@ -24,16 +24,35 @@ namespace AccuFin.Api.Services.BankIntegration
         internal async Task<BankLinkAuthorizationModel> GenerateBankLinkAsyc(string returnUrl, string institution)
         {
             //institution = "SANDBOXFINANCE_SFIN0000";
-            HttpRequestMessage httpRequest = await GetAutherizedHttpRequest("https://ob.nordigen.com/api/v2/requisitions/");
+            //Generate agreement
+            //, max_historical_days = 720, access_valid_for_days = 365 
+
+
+            string agreementId = await GetAgreementIdAsync(institution);
+            var httpRequest = await GetAutherizedHttpRequest("https://ob.nordigen.com/api/v2/requisitions/");
             httpRequest.Method = HttpMethod.Post;
 
             string referenceId = Guid.NewGuid().ToString();
-            string jsonContent = JsonConvert.SerializeObject(new { redirect = returnUrl, institution_id = institution, reference = referenceId });
+            string jsonContent = JsonConvert.SerializeObject(new { redirect = returnUrl, institution_id = institution, reference = referenceId, agreement = agreementId });
             httpRequest.Content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
             var httpClient = _httpClientFactory.CreateClient();
             var result = await httpClient.SendAsync(httpRequest);
             var bankLink = JsonConvert.DeserializeObject<BankLinkAuthorizationModel>(await result.Content.ReadAsStringAsync());
             return bankLink;
+        }
+
+
+        private async Task<string> GetAgreementIdAsync(string institution)
+        {
+            HttpRequestMessage httpRequest = await GetAutherizedHttpRequest("https://ob.nordigen.com/api/v2/agreements/enduser/");
+            httpRequest.Method = HttpMethod.Post;
+            string jsonContent = JsonConvert.SerializeObject(new { institution_id = institution, max_historical_days = 720, access_valid_for_days = 90, access_scope = new string[] { "balances", "details", "transactions" } });
+            httpRequest.Content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            var httpClient = _httpClientFactory.CreateClient();
+            var result = await httpClient.SendAsync(httpRequest);
+            string resultAsJson = await result.Content.ReadAsStringAsync();
+            return (await result.Content.ReadFromJsonAsync<AgreementResultModel>()).Id;
+
         }
 
         private async Task<HttpRequestMessage> GetAutherizedHttpRequest(string url, bool accept = true)
@@ -64,18 +83,19 @@ namespace AccuFin.Api.Services.BankIntegration
             string url = $"https://ob.nordigen.com/api/v2/accounts/{accountId}/transactions/";
             var request = await GetAutherizedHttpRequest(url, false);
             request.Method = HttpMethod.Get;
-  
+
             var result = await httpClient.SendAsync(request);
-           // string json =  await result.Content.ReadAsStringAsync();
+            // string json =  await result.Content.ReadAsStringAsync();
             return await result.Content.ReadFromJsonAsync<TransactionsResultModel>();
         }
-        
+
 
         internal async Task<ICollection<BankModel>> GetBanksAsync()
         {
             HttpClient httpClient = await GetClientForAutherizedRequest();
             var result = await httpClient.GetAsync("https://ob.nordigen.com/api/v2/institutions/?country=nl");
-            return await result.Content.ReadFromJsonAsync<List<BankModel>>();
+            string json = await result.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<BankModel>>(json);
         }
         internal async Task<ListBankAccountResultModel> GetBankAccountsInfoAsync(string accountId)
         {
@@ -109,7 +129,7 @@ namespace AccuFin.Api.Services.BankIntegration
                 string jsonContent = JsonConvert.SerializeObject(new { secret_id = _configuration["nordigen:secret_id"], secret_key = _configuration["nordigen:secret_key"] });
                 httpRequest.Content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
                 var result = await httpClient.SendAsync(httpRequest);
-               _currentToken = await result.Content.ReadFromJsonAsync<SecretResult>();
+                _currentToken = await result.Content.ReadFromJsonAsync<SecretResult>();
             }
             return _currentToken;
         }
